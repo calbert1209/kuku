5c3fead7f914185c7bafd5357064cec5bb3ed068
Christian Albert <calbert1209@gmail.com>
2026-05-07 22:45:20 +0900

    setup build-and-deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: './',
+export default defineConfig(({ command }) => ({
+  base: command === "serve" ? "./" : "/kuku/",
   plugins: [preact()],
-})
+}));
